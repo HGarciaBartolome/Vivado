d154bf9be9a1e5a5d603e2231ac9d1f50071630d
eduardo-luque <61051891+eduardo-luque@users.noreply.github.com>
2023-12-13 15:09:15 +0100

    Add files via upload

---
+++ project_1/project_1.srcs/sources_1/new/FMS_Elevator.vhd
@@ -0,0 +1,120 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity FMS_Elevator is
+ port (
+ RESET: in std_logic;
+ CLK : in std_logic;
+ EDGE : in std_logic_vector(3 DOWNTO 0);
+ MOTORS: OUT std_logic_vector (1 DOWNTO 0); -- 00 stdby 01 Up 10 Down 11 ERROR
+ DOORS: OUT std_logic;  -- 1 Abierto 0 Cerrados
+ LED_Floor: out std_logic_vector(3 DOWNTO 0);
+ LED_EMER: out std_logic
+ );
+end FMS_Elevator;
+
+architecture Behavioral of FMS_Elevator is
+    type STATES is (stdby, Up, Down, Error);
+    signal pisoactual : integer :=0;
+    signal pisoobjetivo: integer := 0;
+    signal trabajando: std_logic:='0';
+    signal state: STATES := stdby;
+    signal tiempoviaje : integer :=0;
+    
+begin
+state_register:process(CLK,RESET)
+    begin
+        if(RESET = '1') then
+            --trabajando <='0';
+        elsif rising_edge(CLK) then
+            if(pisoactual = pisoobjetivo) then
+                state <= stdby;
+            elsif (pisoactual < pisoobjetivo) then
+                state <= Up;
+            elsif (pisoactual > pisoobjetivo) then
+                state <= Down;
+            else
+                state <= Error;
+            end if;
+        end if;
+    end process;
+next_state_decoder: process(EDGE,trabajando)
+    begin
+        if(trabajando = '0') then
+            case EDGE is
+                when "0001" =>
+                   pisoobjetivo<= 0;
+                   trabajando <= '1';
+                when "0010" =>
+                   pisoobjetivo<= 1;
+                   trabajando <= '1';
+                when "0100" =>
+                   pisoobjetivo<= 2;
+                   trabajando <= '1';
+                when "1000" =>
+                   pisoobjetivo<= 3;
+                   trabajando <= '1';
+                when others =>
+            end case;
+        end if;
+    end process;
+output_decod: process(pisoactual,state)
+    begin
+        if (state = stdby) then
+            MOTORS(1) <= '0';
+            MOTORS(0) <= '0';
+            DOORS <= '1';
+        elsif (state = Up) then
+            MOTORS(1) <= '0';
+            MOTORS(0) <= '1';
+            DOORS <= '0';
+        elsif (state = Down) then
+            MOTORS(1) <= '1';
+            MOTORS(0) <= '0';
+            DOORS <= '0';
+        elsif (state = Error) then
+            MOTORS(1) <= '0';
+            MOTORS(0) <= '0';
+        end if;
+        case pisoactual is
+            when 0 =>
+                LED_floor <= "0001";
+            when 1 =>
+                LED_floor <= "0010";
+            when 2 =>
+                LED_floor <= "0100";
+            when 3 =>
+                LED_floor <= "1000";
+              when others =>
+        end case;
+    end process;
+    
+error_detect: process(pisoactual,pisoobjetivo,state) --comprobación de error en la situación actual del ascensor, si baja más de lo que debería o si sube más de lo debido
+    begin
+        if(pisoactual<pisoobjetivo) then
+            if(state=down) then
+           -- state<=Error;
+            LED_EMER<='1';
+            end if;
+        elsif(pisoactual>pisoobjetivo) then
+            if(state=up) then
+           -- state<=Error;
+            LED_EMER<='1';
+            end if;
+        end if;
+    end process;
+
+cambio_piso: process(pisoactual,pisoobjetivo,CLK)--se mueve al piso indicado, esperando x tiempo simulando el movimiento del ascensor
+    begin
+        if(state = Up) then
+
+            pisoactual<=pisoactual+1;--tras terminar la espera, sube un piso
+        
+        elsif(state = Down) then
+
+            pisoactual<=pisoactual-1;--tras terminar la espera, sube un piso
+            
+        elsif(state = stdby) then
+        end if;
+    end process;
+end Behavioral;
---
+++ project_1/project_1.srcs/sources_1/new/Top.vhd
@@ -0,0 +1,98 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity Top is
+ PORT ( 
+ RESET: in std_logic;
+ Button: in std_logic_vector(3 DOWNTO 0);
+ CLK : in std_logic;
+ EMER : in std_logic;
+ LED_Floor : OUT std_logic_vector(3 DOWNTO 0);
+ MOTORS: OUT std_logic_vector (1 DOWNTO 0); -- 00 stdby 01 Up 10 Down 11 ERROR
+ DOORS: OUT std_logic;  -- 1 Abierto 0 Cerrados
+ EMER_LED : OUT std_logic
+
+ );
+end Top;
+
+architecture Behavioral of Top is
+    signal SSYNC : std_logic_vector(3 DOWNTO 0);
+    signal EDGES : std_logic_vector(3 DOWNTO 0);
+COMPONENT SYNCHRNZR 
+ port (
+ CLK : in std_logic;
+ ASYNC_IN : in std_logic;
+ SYNC_OUT : out std_logic
+ );
+END COMPONENT;
+COMPONENT EDGEDTCTR
+ port (
+ CLK : in std_logic;
+ SYNC_IN : in std_logic;
+ EDGE : out std_logic
+ );
+END COMPONENT;
+COMPONENT FMS_Elevator
+ port (
+ RESET: in std_logic;
+ CLK : in std_logic;
+ EDGE : in std_logic_vector(3 DOWNTO 0);
+ MOTORS: OUT std_logic_vector (1 DOWNTO 0); -- 00 stdby 01 Up 10 Down 11 ERROR
+ DOORS: OUT std_logic;  -- 1 Abierto 0 Cerrados
+ LED_Floor: out std_logic_vector(3 DOWNTO 0);
+ LED_EMER: out std_logic
+ );
+END COMPONENT;
+
+begin
+
+Inst_synchrnzr0:  SYNCHRNZR port MAP(
+ CLK =>CLK,
+ ASYNC_IN => Button(0),
+ SYNC_OUT => SSYNC(0)
+ );
+ Inst_edgedtctr0: EDGEDTCTR PORT MAP (
+ CLK => CLK,
+ SYNC_IN=>SSYNC(0),
+ EDGE=>EDGES(0)
+);
+Inst_synchrnzr1:  SYNCHRNZR port MAP(
+ CLK =>CLK,
+ ASYNC_IN => Button(1),
+ SYNC_OUT => SSYNC(1)
+ );
+ Inst_edgedtctr1: EDGEDTCTR PORT MAP (
+ CLK => CLK,
+ SYNC_IN=>SSYNC(1),
+ EDGE=>EDGES(1)
+);
+Inst_synchrnzr2:  SYNCHRNZR port MAP(
+ CLK =>CLK,
+ ASYNC_IN => Button(2),
+ SYNC_OUT => SSYNC(2)
+ );
+ Inst_edgedtctr2: EDGEDTCTR PORT MAP (
+ CLK => CLK,
+ SYNC_IN=>SSYNC(2),
+ EDGE=>EDGES(2)
+);
+Inst_synchrnzr3:  SYNCHRNZR port MAP(
+ CLK =>CLK,
+ ASYNC_IN => Button(3),
+ SYNC_OUT => SSYNC(3)
+ );
+ Inst_edgedtctr3: EDGEDTCTR PORT MAP (
+ CLK => CLK,
+ SYNC_IN=>SSYNC(3),
+ EDGE=>EDGES(3)
+);
+Inst_fmsElevator: FMS_Elevator PORT MAP(
+ RESET => RESET,
+ CLK => CLK,
+ EDGE => EDGES,
+ DOORS =>DOORS,
+ MOTORS => MOTORS,
+ LED_Floor => LED_Floor,
+ LED_EMER => EMER_LED
+);
+end Behavioral;
